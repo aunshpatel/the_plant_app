7d4081cd8ab092d24dddabb1e847dce71530c548
aunshpatel <patelaunsh28@gmail.com>
2024-09-15 03:14:54 -0400

    Update model

--- lib/models/plant.dart
+++ lib/models/plant.dart
@@ -1,20 +1,32 @@
 class Plant {
   final String id;
-  final String name;
-  // final String type;
+  final String categoryID;
   final String imageUrl;
+  final String name;
+  final String rating;
+  final String displaySize;
+  final List availableSize;
+  final String unit;
+  final String price;
+  final String priceUnit;
   final String description;
 
   // Plant({required this.id, required this.name, required this.type, required this.imageUrl, required this.description});
-  Plant({required this.id, required this.name, required this.imageUrl, required this.description});
+  Plant({required this.id, required this.categoryID, required this.imageUrl, required this.name, required this.rating, required this.displaySize, required this.availableSize, required this.unit, required this.price, required this.priceUnit, required this.description});
 
   factory Plant.fromJson(Map<String, dynamic> json) {
     // print("json:$json");
     return Plant(
       id: json['id'].toString(),
-      name: json['name'].toString(),
-      // type: json['type'].toString(),
+      categoryID: json['category_id'].toString(),
       imageUrl: json['image_url'].toString(),
+      name: json['name'].toString(),
+      rating: json['rating'].toString(),
+      displaySize: json['display_size'].toString(),
+      availableSize: json['available_size'],
+      unit: json['unit'].toString(),
+      price: json['price'].toString(),
+      priceUnit: json['price_unit'].toString(),
       description: json['description'].toString(),
     );
   }
