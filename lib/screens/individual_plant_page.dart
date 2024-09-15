4d1304f267d0b84f3bed987382bc904c0343eb18
aunshpatel <patelaunsh28@gmail.com>
2024-09-15 18:36:24 -0400

    Create base UI for plant page

--- lib/screens/individual_plant_page.dart
+++ lib/screens/individual_plant_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:the_plant_app/screens/widgets/consts.dart';
 import '../models/plant.dart';
 
 class PlantDetailPage extends StatelessWidget {
@@ -6,19 +7,85 @@ class PlantDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final Plant plant = ModalRoute.of(context)!.settings.arguments as Plant;
     return Scaffold(
-      appBar: AppBar(title: Text(plant.name)),
+      appBar: AppBar(),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Image.network(plant.imageUrl),
-            SizedBox(height: 16),
-            Text(plant.name, style: TextStyle(fontSize: 24)),
-            SizedBox(height: 8),
-            // Text(plant.type, style: TextStyle(fontSize: 16)),
-            SizedBox(height: 8),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(plant.name, style: const TextStyle(fontSize: 24)),
+                Row(
+                  children: [
+                    Text(plant.price, style: const TextStyle(fontSize: 24)),
+                    Text(plant.priceUnit, style: const TextStyle(fontSize: 24)),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                const Icon(Icons.star, color: Color(0XFFFFD700)),
+                Text(plant.rating, style: kGoldTextStyle,)
+              ],
+            ),
+            const SizedBox(height: 18),
+            const Text('Choose Size', style: kHeaderStyle,),
+            const SizedBox(height: 8),
+            Container(
+              height: 50,
+              child: Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: plant.availableSize.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Container(
+                        decoration: kSelectedPlantSizeButton,
+                        child:MaterialButton(
+                          onPressed: null,
+                          child: Text('${plant.availableSize[index].toString()} ${plant.unit}', style: kSelectedPlantSizeText,),
+                        ),
+                      ),
+                    );
+                  }
+                )
+              ),
+            ),
+            const SizedBox(height: 18),
+            const Text('Description', style: kHeaderStyle,),
+            const SizedBox(height: 8),
             Text(plant.description),
+            const SizedBox(height: 18),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Center(
+                    child: Icon(Icons.favorite_border, size: 50,),
+                  )
+                ),
+                Container(
+                  decoration: kSelectedPlantSizeButton,
+                  width: 200,
+                  child:MaterialButton(
+                    onPressed: null,
+                    child: Text('Add to cart', style: kSelectedPlantSizeText,),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
--- lib/screens/widgets/consts.dart
+++ lib/screens/widgets/consts.dart
@@ -10,3 +10,20 @@ const kGoldTextStyle = TextStyle(
   fontWeight: FontWeight.w600,
   color: Color(0XFFFFD700),
 );
+
+const kHeaderStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+const kSelectedPlantSizeText = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+var kSelectedPlantSizeButton = BoxDecoration(
+  border: Border.all(width: 2.0, color: Color(0XFFA58787)),
+  borderRadius: BorderRadius.circular(16),
+  color: const Color(0XFFA58787),
+);
